abfc64b313f0bbc825dc90918dc4ad26716a5745
Sarav Ramaswamy <saravr@yahoo.com>
2013-07-14 11:50:31 -0700

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btlgs::Application.config.secret_token = '9227f1fb22eb2fab7ad0b7371461ec2a644214d5f4cc3671cb2e889d1fd96564168546940df3588031fe0bb5a230e487059ccfea4fde1a33cbbcfea5028cfa33'
